a5db0c2ee15eb6e08ecbe3bfea2d1d216f80509a
Dovlyat <102499378+Davlya@users.noreply.github.com>
2022-06-02 10:46:58 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 
 from tkinter import *
-from tkinter import messagebox as mb
 from tkinter.ttk import Notebook
 
 from PIL import Image, ImageOps
@@ -8,7 +7,7 @@ from PIL import Image, ImageOps
 import os
 
 
-class CapybaraPhoto:
+class Photo_editor:
     def __init__(self):
         self.root = Tk()
         self.label = Label(self.root, text="Choose a Button")
@@ -17,30 +16,24 @@ class CapybaraPhoto:
         self.count_bw = 1
         self.count_r = 1
         self.count_l = 1
-        self.count_u = 1
-        self.count_m = 1
-        self.count_n = 1
-        self.mes = "Done! Don't close the program window, just check the folder"
         self.init()
-
+# Windows settings 
     def init(self):
         self.root.geometry('400x400')
-        
-        # you can change title name here 
         self.root.title('Photo editor')
         self.image_tabs.enable_traversal()
 
-
+# Method thta creates windows and call buttons
     def window(self):
         self.buttons()
         self.root.mainloop()
 
 
-    #Creating buttons and setting commands
+    #Method that creates buttoms in TKinter Window
     def buttons(self):
-        bw_filter = Button(self.root, width=18, height=5, text="Apply a BW filter", command=self.bw_fil)
-        rotate_right = Button(self.root, width=18, height=5, text="Rotate image right", command=self.right_com)
-        rotate_left = Button(self.root, width=18, height=5, text="Rotate image left", command=self.left_com)
+        bw_filter = Button(self.root, width=18, height=5, text="Apply a BW filter", bg = 'grey', command=self.bw_fil)
+        rotate_right = Button(self.root, width=18, height=5, text="Rotate image right", bg='red', command=self.right_com)
+        rotate_left = Button(self.root, width=18, height=5, text="Rotate image left", bg='blue', command=self.left_com)
         bw_filter.grid(row=1, column=0)
         rotate_right.grid(row=1, column=2)
         rotate_left.grid(row=1, column=3)
@@ -50,7 +43,7 @@ class CapybaraPhoto:
     
 
     
-
+#Method that applies b/w filter
     def bw_fil(self):
         os.mkdir(f"bw filter {str(self.count_bw)}")
         folder_name = (f'bw filter {str(self.count_bw)}'+'/{}_bw{}')
@@ -64,7 +57,7 @@ class CapybaraPhoto:
         
         self.count_bw += 1
 
-
+#Method for rotating images to the Right
     def right_com(self):
         #There is I created new folder
         os.mkdir(f"rotate right {str(self.count_r)}")
@@ -80,7 +73,7 @@ class CapybaraPhoto:
 
 
     
-    
+# Rotating images to the Left
     def left_com(self):
         os.mkdir(f"rotate left {str(self.count_l)}")
         folder_name = (f'rotate left {str(self.count_l)}'+'/{}_left{}')
@@ -97,4 +90,4 @@ class CapybaraPhoto:
 
 
 if __name__ == "__main__":
-    CapybaraPhoto().window()
+    Photo_editor().window()
